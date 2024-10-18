a1969c195413c8e67536cb41f9181fea9e217f19
danuumalespina <danuumalespina@gmail.com>
2024-10-18 21:22:26 +0100

    third

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,34 +19,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         User::create([
-            'name' => "Alexander Texas",
-            'email' => "alexander@gmail.com",
-            'role' => "drop_manager",
-            'password' => Hash::make("alexanderalexander"),
-        ]);
-
-        User::create([
-            'name' => "Noran Appiggo",
-            'email' => "appiggo@gmail.com",
-            'role' => "company_creator",
-            'password' => Hash::make("appiggoappiggo"),
-        ]);
-
-        User::create([
-            'name' => "Stannis Mahon",
-            'email' => "mahon@gmail.com",
-            'role' => "website_creator",
-            'password' => Hash::make("mahonmahon"),
-        ]);
-
-        User::create([
-            'name' => "Norax Lom",
-            'email' => "norax@gmail.com",
-            'role' => "support",
-            'password' => Hash::make("noraxnorax"),
-        ]);
-
-        User::create([
             'name' => "madmans",
             'email' => "madmans@gmail.com",
             'role' => "admin",
